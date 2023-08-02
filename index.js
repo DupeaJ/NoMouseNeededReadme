6f230c929348a2ca47c38c10517cca91ca912fec
unknown <jacobdupea@gmail.com>
2023-08-02 16:34:07 -0700

    added questions and spots for the answers to go

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generateREADME = ({ ProjectName, Description, Username, license, steps }) =>
-
-`# ${ProjectName}
+const generateREADME = ({
+    ProjectName,
+    Description,
+    Username,
+    license,
+    steps,
+    Usage,
+    Contributing,
+    Tests,
+    Email,
+    Credits,
+    Features,
+}) =>
+    `# ${ProjectName}
 ## Description
 ${Description}
 ## Table of Contents
 
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
+- [Installation](##installation)
+- [Usage](##usage)
+- [Credits](##credits)
+- [License](##license)
 
 ## Installation
 
@@ -19,8 +30,10 @@ steps required
 ${steps}
 
 ## Usage
+${Usage}
 
 ## Credits
+${Credits}
 
 ## License
 refer to repo for licenses
@@ -30,10 +43,17 @@ license is a ${license} license.
 ![badge](https://img.shields.io/badge/License-${license}-blue.svg)
 
 ## Features
+${Features}
 
 ## How to Contribute
+${Contributing}
 
 ## Tests
+${Tests}
+
+## Questions
+reach my git hub profile at (https://github.com/${Username})
+If you have additional questions you can reach me at ${Email}
     `;
 
 inquirer
@@ -57,13 +77,43 @@ inquirer
             type: "list",
             message: "what license is your project",
             name: "license",
-            choices: ["MIT", "MPL", "ISC","IBM"],
+            choices: ["MIT", "MPL", "ISC", "IBM"],
         },
         {
-        type: "input",
-        message: "what are the steps to install your project?",
-        name: "steps",
-        }
+            type: "input",
+            message: "what are the steps to install your project?",
+            name: "steps",
+        },
+        {
+            type: "input",
+            message: "how can your project be useful?",
+            name: "Usage",
+        },
+        {
+            type: "input",
+            message: "How can people contribute",
+            name: "Contributing",
+        },
+        {
+            type: "input",
+            message: "what are the steps to install your project?",
+            name: "Tests",
+        },
+        {
+            type: "input",
+            message: "how do you want people to reach you about issues(email)",
+            name: "Email",
+        },
+        {
+            type: "input",
+            message: "what special credits do you want to give",
+            name: "Credits",
+        },
+        {
+            type: "input",
+            message: "what special features does your project have",
+            name: "Features",
+        },
     ])
     .then((answers) => {
         const readmePage = generateREADME(answers);
@@ -73,13 +123,7 @@ inquirer
                 : console.log("Successfully created READEME!")
         );
     });
-        
-        
-        
-        
-        
-        
-        
+
 //const fileName = "README.md"
 // function writeFile(fileName, data) {
 //         console.log(fileName)
